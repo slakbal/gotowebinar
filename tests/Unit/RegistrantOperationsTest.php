7c3107bb31251bc753495b79b98c58ed0406b8f6
Jesse Vista <jvista@upwebdesign.com>
2019-02-04 16:42:01 -0500

    Written test containers for Registrant and Session Operations

---
+++ tests/Unit/RegistrantOperationsTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Slakbal\Gotowebinar\Test\Unit;
+
+use Slakbal\Gotowebinar\Test\BaseTestCase;
+use Slakbal\Gotowebinar\Facade\GotoWebinar;
+
+class RegistrantOperationsTest extends BaseTestCase
+{
+}
